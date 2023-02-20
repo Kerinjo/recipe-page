66e484dea74f0a6a4aeca31cdad44a31d405bf13
Kerinjo <konmarciniak@gmail.com>
2023-02-20 22:29:22 +0100

    Cleanup, calling api with axios

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import axios from 'axios'
+
+axios.get('http://www.themealdb.com/api/json/v1/1/random.php')
+  .then(response => {
+    const meals = response.data
+    console.log(meals)
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
