07c0f51864537c88c7e9e697ae78ed995a40af9b
Kerinjo <konmarciniak@gmail.com>
2023-03-03 20:39:33 +0100

    measure added alongside ingredient

--- src/components/RandomRecipe.jsx
+++ src/components/RandomRecipe.jsx
@@ -14,18 +14,32 @@ const Recipe = () => {
   // rogaliki: www.themealdb.com/api/json/v1/1/lookup.php?i=53024
 
   try {
-    const iterator = Object.keys(recipe.meals[0]).filter(word => word.includes("strIngredient"))
-    const ingredients = iterator
+    const ing_iterator = Object.keys(recipe.meals[0])
+      .filter(word => word.includes("strIngredient"))
+    const ingredients = ing_iterator
       .map((element) => recipe.meals[0][element])
-      .filter((element)=>element !== "")
+      .filter((element)=> element !== "")
+
+    const meas_iterator = Object.keys(recipe.meals[0])
+      .filter(word => word.includes("strMeasure"))
+    const measures = meas_iterator
+      .map((element) => recipe.meals[0][element])
+      .filter((element) => element !== "")
+
+    const ingredientArray = []
+    for (let i = 0; i < ingredients.length; i++) {
+      ingredientArray.push([i, ingredients[i], measures[i]])
+    } 
+ 
     return (
       <div>
         <img width={300} src={recipe.meals[0].strMealThumb}></img>
         <h1>{recipe.meals[0].strMeal}</h1>
         <p>{recipe.meals[0].strInstructions}</p>
         <h2>Ingredients</h2>
-        <ul>
-          {ingredients.map(ingredient => <li key={ingredients.indexOf(ingredient)}>{ingredient}</li>)}
+        <ul>         
+           {ingredientArray.map(ingredient =>
+             <li key={ingredient[0]}>{ingredient[1]} {ingredient[2]}</li>)}
         </ul>
       </div>
     )
